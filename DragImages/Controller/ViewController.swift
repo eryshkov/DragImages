cb2501395573ec2a16df8d1e762cd7560a1301cf
Evgenii Ryshkov <eryshkov@gmail.com>
2019-01-28 12:37:23 +0300

    performDrop is implemented

--- DragImages/Controller/ViewController.swift
+++ DragImages/Controller/ViewController.swift
@@ -12,9 +12,38 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.addInteraction(UIDropInteraction(delegate: self))
     }
 
 
 }
 
+extension ViewController: UIDropInteractionDelegate {
+    func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
+        return session.canLoadObjects(ofClass: UIImage.self)
+    }
+    
+    func dropInteraction(_ interaction: UIDropInteraction, sessionDidUpdate session: UIDropSession) -> UIDropProposal {
+        return UIDropProposal(operation: .copy)
+    }
+    
+    func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
+        for dragItem in session.items {
+            dragItem.itemProvider.loadObject(ofClass: UIImage.self) {[unowned self] (obj, error) in
+                if let err = error {
+                    print("Failed to load our dragged item: \(err.localizedDescription)")
+                    return
+                }
+                
+                guard let draggedImage = obj as? UIImage else {return}
+                
+                DispatchQueue.main.async {
+                    let imageView = UIImageView(image: draggedImage)
+                    self.view.addSubview(imageView)
+                    imageView.frame = CGRect(x: 0, y: 0, width: draggedImage.size.width, height: draggedImage.size.height)
+                }
+            }
+        }
+    }
+}
